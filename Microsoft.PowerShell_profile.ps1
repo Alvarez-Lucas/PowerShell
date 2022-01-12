41d0ea6a2d14450a94dadd9a8e7e76238dd07f68
Lucas <lucasalvarez@pm.me>
2022-01-12 16:05:19 -0500

    Initial Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+# Prompt
+Import-Module oh-my-posh
+#oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+Set-PoshPrompt Paradox
+
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+
+# PSReadLine
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+
+# FuzzyFinder
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+# Set-Alias l lfcd
+
+
+Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert('l.ps1')
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
