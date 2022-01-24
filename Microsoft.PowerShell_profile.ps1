2615c4a12f78e3d3973838c29129dd5d3f2fcda3
lta3301 <lta3301@g.rit.edu>
2022-01-24 12:38:28 -0500

    Change theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@
 Import-Module oh-my-posh
 #oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
 # Set-PoshPrompt Paradox
-Set-PoshPrompt bubbles
+Set-PoshPrompt spaceship
 
 
 # Icons
